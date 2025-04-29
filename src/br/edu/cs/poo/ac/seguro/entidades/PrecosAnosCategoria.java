4992bbeedec9aef9a3a46adaa780eb1a05bc8104
JuliaTBarros <162466823+JuliaTBarros@users.noreply.github.com>
2025-04-29 20:13:35 -0300

    Refactor CPF/CNPJ validation and remove unused files.
    Simplified CPF/CNPJ validation logic by extracting common steps into helper methods and removing hardcoded test cases. Deleted unused PersistenciaObjetos.xml and associated JAR file. Corrected minor formatting issues across the codebase for consistency.

--- src/br/edu/cs/poo/ac/seguro/entidades/PrecosAnosCategoria.java
+++ src/br/edu/cs/poo/ac/seguro/entidades/PrecosAnosCategoria.java
@@ -7,21 +7,21 @@ class PrecosAnosCategoria {
             new PrecoAno(2022, 60000.0),
             new PrecoAno(2023, 64000.0),
             new PrecoAno(2024, 68000.0),
-            new PrecoAno(2025, 72000.0) };
+            new PrecoAno(2025, 72000.0)};
     static final PrecoAno[] PA_INTERMEDIARIO = {
             new PrecoAno(2020, 60000.0),
             new PrecoAno(2021, 69000.0),
             new PrecoAno(2022, 75000.0),
             new PrecoAno(2023, 86000.0),
             new PrecoAno(2024, 93000.0),
-            new PrecoAno(2025, 100000.0) };
+            new PrecoAno(2025, 100000.0)};
     static final PrecoAno[] PA_LUXO = {
             new PrecoAno(2020, 70000.0),
             new PrecoAno(2021, 82000.0),
             new PrecoAno(2022, 88000.0),
             new PrecoAno(2023, 99000.0),
             new PrecoAno(2024, 110000.0),
-            new PrecoAno(2025, 120000.0) };
+            new PrecoAno(2025, 120000.0)};
     static final PrecoAno[] PA_SUPER_LUXO = {
             new PrecoAno(2020, 90000.0),
             new PrecoAno(2021, 105000.0),
@@ -36,6 +36,6 @@ class PrecosAnosCategoria {
             new PrecoAno(2022, 140000.0),
             new PrecoAno(2023, 170000.0),
             new PrecoAno(2024, 200000.0),
-            new PrecoAno(2025, 246000.0) };
+            new PrecoAno(2025, 246000.0)};
 
 }
